1f50c41529423619a0cb45f09baef9f4e3e69078
Carlos García Gómez <neoazorx@gmail.com>
2021-12-22 12:59:56 +0100

    Modificado el cron para solamente procesar 50 proyectos cada 30 minutos cuando hay más de 100 proyectos.

--- Cron.php
+++ Cron.php
@@ -36,9 +36,14 @@ class Cron extends CronClass
     public function run()
     {
         if ($this->isTimeForJob('project-stock-update', '30 minutes')) {
-            $limit = 10;
-            $offset = $limit * (int)date('H');
+            $limit = 0;
+            $offset = 0;
             $projectModel = new Proyecto();
+            if ($projectModel->count() > 100) {
+                $limit = 50;
+                $offset = min([$limit * mt_rand(0, 4), $projectModel->count() - $limit]);
+            }
+
             foreach ($projectModel->all([], ['idproyecto' => 'DESC'], $offset, $limit) as $project) {
                 ProjectStockManager::rebuild($project->idproyecto);
                 ProjectTotalManager::recalculate($project->idproyecto);
