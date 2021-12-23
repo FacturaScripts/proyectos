bf719a00b6a2073abf302e342b60dca0f9a2f930
Carlos García Gómez <neoazorx@gmail.com>
2021-12-23 13:43:53 +0100

    Modificado el cron para volver a procesar todos los proyectos en cada ciclo.

--- Cron.php
+++ Cron.php
@@ -35,16 +35,9 @@ class Cron extends CronClass
 
     public function run()
     {
-        if ($this->isTimeForJob('project-stock-update', '30 minutes')) {
-            $limit = 0;
-            $offset = 0;
+        if ($this->isTimeForJob('project-stock-update', '10 minutes')) {
             $projectModel = new Proyecto();
-            if ($projectModel->count() > 100) {
-                $limit = 50;
-                $offset = min([$limit * mt_rand(0, 4), $projectModel->count() - $limit]);
-            }
-
-            foreach ($projectModel->all([], ['idproyecto' => 'DESC'], $offset, $limit) as $project) {
+            foreach ($projectModel->all([], ['idproyecto' => 'DESC'], 0, 0) as $project) {
                 ProjectStockManager::rebuild($project->idproyecto);
                 ProjectTotalManager::recalculate($project->idproyecto);
             }
