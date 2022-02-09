7ddb2a9d2a3ed7692d09d2a458c06a47a1c96657
Carlos Garcia Gomez <neorazorx@gmail.com>
2022-02-09 12:26:51 +0100

    Añadido código para limpiar la caché al recalcular los stocks y totales de los proyectos.

--- Cron.php
+++ Cron.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of Proyectos plugin for FacturaScripts
- * Copyright (C) 2020-2021 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2020-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -48,6 +48,9 @@ class Cron extends CronClass
             }
 
             $this->jobDone('project-stock-update');
+
+            // limpiamos la caché para forzar refrescar los totales de los listados
+            $this->toolBox()::cache()->clear();
         }
     }
 
