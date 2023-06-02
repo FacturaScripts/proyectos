e567fe8da4ca38f6591d2479ba40bf324dd51b46
Carlos Garcia Gomez <neorazorx@gmail.com>
2023-06-02 17:51:50 +0200

    Actualizada la versión y las traducciones.

--- Controller/EditProyecto.php
+++ Controller/EditProyecto.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of Proyectos plugin for FacturaScripts
- * Copyright (C) 2020-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2020-2023 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -20,6 +20,7 @@
 namespace FacturaScripts\Plugins\Proyectos\Controller;
 
 use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
+use FacturaScripts\Core\Cache;
 use FacturaScripts\Core\Lib\ExtendedController\EditController;
 use FacturaScripts\Core\Lib\ExtendedController\EditView;
 use FacturaScripts\Core\Model\Base\ModelCore;
@@ -427,7 +428,7 @@ class EditProyecto extends EditController
             ProjectTotalManager::recalculate($idproyecto);
 
             // limpiamos caché para forzar actualizar los totales de los listados
-            self::toolBox()::cache()->clear();
+            Cache::clear();
             self::toolBox()::i18nLog()->notice('project-stock-rebuild-ok');
             return;
         }
