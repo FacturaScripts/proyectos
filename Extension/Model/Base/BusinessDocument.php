bccb1eb1f78bc6d4e6cebdef8dedcb363d640c09
Carlos Garcia Gomez <neorazorx@gmail.com>
2023-12-13 19:09:57 +0100

    Corregido soporte de extensiones en los BusinessDocument.

--- Extension/Model/Base/BusinessDocument.php
+++ Extension/Model/Base/BusinessDocument.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of Proyectos plugin for FacturaScripts
- * Copyright (C) 2020-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2020-2023 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -54,10 +54,10 @@ class BusinessDocument
         };
     }
 
-    protected function setPreviousDataMore(): Closure
+    public function setPreviousData(): Closure
     {
         return function () {
-            return ['idproyecto'];
+            $this->previousData['idproyecto'] = $this->idproyecto ?? null;
         };
     }
 }
